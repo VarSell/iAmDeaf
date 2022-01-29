d733bac3d24a2766a367f5d4493ba2519ee33a41
Acaelus Thorne <acaelusthorne6430@gmail.com>
2022-01-29 12:13:30 +0100

    Added multithreading, used for cue generation

--- iAmDeaf/Program.cs
+++ iAmDeaf/Program.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Globalization;
 using System.Configuration;
+using System.Threading;
 
 
 namespace Workings
@@ -20,10 +21,8 @@ namespace Workings
 
     class Methods
     {
-
         public static string nfo(string aax, string m4b)
         {
-
             string[] nfoPart = new string[15];
             string mi = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe";
             nfoPart[0] = SoftWare(mi, $"{aax} --Inform=General;%Album%", false); //Title
@@ -60,7 +59,6 @@ namespace Workings
             }
             nfoPart[14] = SoftWare(mi, $"{aax} --Inform=General;%Track_More%", false); //comment
 
-
             string nfo = @$"General Information
 ===================
  Title:                  {nfoPart[0].Trim()}
@@ -95,7 +93,7 @@ Publisher's Summary
         {
             string mi = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe";
             string[] info = new string[5];
-
+            
             info[0] = SoftWare(mi, $"{aax} --Inform=General;%Album%", false);
             info[1] = info[0].Split(",").Last().Trim();
             info[2] = info[0].Replace(info[1], null).Replace(",", null).Split(":").Last().Trim();
@@ -137,36 +135,38 @@ Publisher's Summary
         }
 
 
-        public static void cue(string m4b, string file)
+        /*public static void cue(string m4b, string file) //UNTHREADED METHOD
         {
             Alert("Generating cue");
             SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $" -i \"{m4b}.m4b\" -c copy {AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv -y", true);
             SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mkvextract.exe", $" {AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv chapters -s {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt", true);
-
-            //cleanup of cue method
             File.Delete($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv");
-
             string cuegen = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\cuegen.vbs {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt";
-
             Process.Start(@"cmd", @"/c " + cuegen);
-            
             string[] cue = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.cue");
-            
             cue[0] = $"FILE \"{Path.GetFileName($"{file}.m4b")}\" MP4";
             File.WriteAllLines($"{file}.cue", cue);
+        }*/
 
-            
+        public static void cueGenTHR(string aax, string file) //THREADED METHOD
+        {
+            Alert("Generating cue");
+            SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $" -i \"{aax}\" -c copy {AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv -y", true);
+            SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mkvextract.exe", $" {AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv chapters -s {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt", true);
+            File.Delete($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv");
+            string cuegen = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\cuegen.vbs {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt";
+            Process.Start(@"cmd", @"/c " + cuegen);
+            string[] cue = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.cue");
+            cue[0] = $"FILE \"{Path.GetFileName($"{file}.m4b")}\" MP4";
+            File.WriteAllLines($"{file.Replace("\"", "")}.cue", cue);
         }
-
 
         public static string getBytes(string aax)
         {
-
             string checksum = SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffprobe.exe", $"{aax}", true);
             File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\_temp.txt", checksum.Replace(" ", ""));
             string[] line = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\_temp.txt");
             checksum = (line[11].Split("==").Last());
-            
 
             Alert($"Checksum: {checksum}");
 
@@ -175,14 +175,11 @@ Publisher's Summary
              */
 
             Directory.SetCurrentDirectory($"{AppDomain.CurrentDomain.BaseDirectory}src\\tables");
-
             string bytes = SoftWare($"rcrack.exe", $" . -h {checksum}", false);
-
             File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\bytes.txt", bytes.Replace(" ", ""));
             line = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\bytes.txt");
             bytes = (line[32].Split("hex:").Last());
             File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\bytes.txt", bytes);
-
             Alert($"ActBytes: {bytes}");
             return bytes;
         }
@@ -195,10 +192,8 @@ Publisher's Summary
             SoftWare.StartInfo.FileName = @$"{software}";
             SoftWare.StartInfo.Arguments = $" {arguments} ";
 
-
             if (std == true)
             {
-
                 SoftWare.StartInfo.RedirectStandardError = true;
                 SoftWare.StartInfo.StandardErrorEncoding = Encoding.UTF8;
                 SoftWare.Start();
@@ -235,8 +230,6 @@ namespace Main
         {
             string aax = null;
 
-            
-
             if (args.Length > 0)
             {
 
@@ -245,43 +238,41 @@ namespace Main
                     aax = obj.ToString();
                     aax = $"\"{aax}\"";
                 }
+
+                /*if(!File.Exists(aax))
+                {
+                    Workings.Methods.AlertError("Invalid FilePath.");
+                    return 0;
+                }*/
             }
 
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("No command line arguments found.");
-                Console.ResetColor();
+                Workings.Methods.AlertError("No args provided.");
                 return 0;
             }
 
-
-            
-
             Workings.Methods.Alert("Parsing File");
-
-
-
-            string bytes = Workings.Methods.getBytes(aax);
-
             
             string[] filename = Workings.Methods.MediaInfo(aax);
             string title = filename[0];
             filename[0] = filename[0].Trim().Replace(":", " -");
-            //System.IO.Directory.CreateDirectory(filename[0]);
-            string file = ($@"{filename[2]} [{filename[1]}] {filename[3]}"); // add an icon
+            string file = ($@"{filename[2]} [{filename[1]}] {filename[3]}");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(file);
             Console.ResetColor();
             string comment = filename[4].Trim();
 
+            string bytes = Workings.Methods.getBytes(aax);
 
             //*** create AAX Host dir HERE before ffmpeg merge
 
             string hostDir = Path.GetDirectoryName(aax).Replace("\"", "");
             file = $"\"{hostDir}\\{filename[0]}\\{file.Trim()}";
-
             System.IO.Directory.CreateDirectory($"{hostDir}\\{filename[0]}");
+
+            Thread THR = new Thread(() => Workings.Methods.cueGenTHR($"{aax}", $"{file.Replace("\"", "")}"));
+            THR.Start();
 
             Workings.Methods.Alert("Writing..");
             Workings.Methods.SoftWare($"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $"-activation_bytes {bytes} -i {aax} -metadata:g encoding_tool=\"{Workings.iAmDeaf.mark} {Workings.iAmDeaf.version}\" -metadata title=\"{title}\" -metadata comment=\"{comment}\" -c copy {file}.m4b\" -y", true);
@@ -290,16 +281,14 @@ namespace Main
             Workings.Methods.SoftWare($"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $"-i {aax} -map 0:v -map -0:V -c copy {file}.jpg\" -y", true);
             string nfo = Workings.Methods.nfo(aax, $"{file}.m4b\"");
 
-            Workings.Methods.cue($"{file.Replace("\"", "")}", $"{file.Replace("\"", "")}");
+            //Workings.Methods.cue($"{file.Replace("\"", "")}", $"{file.Replace("\"", "")}");
+
+            THR.Join();
 
             Workings.Methods.Alert("Generating nfo");
             File.WriteAllText(($"{file}.nfo\"").Replace("\"", ""), nfo, Encoding.UTF8);
 
-
-            
-
             return 0;
-
         }
     }
 }
