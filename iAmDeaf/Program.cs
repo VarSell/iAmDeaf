49cf1faa352c782b3b65829ef26b1b6fe4a18582
Acaelus Thorne <acaelusthorne6430@gmail.com>
2022-01-31 19:07:16 +0100

    renamed some things

--- iAmDeaf/Program.cs
+++ iAmDeaf/Program.cs
@@ -16,7 +16,7 @@ namespace Workings
     static class iAmDeaf
     {
         public const string mark = "iAmDeaf";
-        public const string version = "1.4";
+        public const string version = "1.4.2";
     }
 
     class Methods
@@ -24,40 +24,48 @@ namespace Workings
         public static string nfo(string aax, string m4b)
         {
             string[] nfoPart = new string[15];
-            string mi = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe";
-            nfoPart[0] = SoftWare(mi, $"{aax} --Inform=General;%Album%", false); //Title
-            nfoPart[1] = SoftWare(mi, $"{aax} --Inform=General;%Performer%", false); //Author
-            nfoPart[2] = SoftWare(mi, $"{aax} --Inform=General;%nrt%", false); //Narrator
-            nfoPart[3] = SoftWare(mi, $"{aax} --Inform=General;%Copyright%", false); //Copyright
-            nfoPart[4] = SoftWare(mi, $"{aax} --Inform=General;%Genre%", false); //Genre
-            nfoPart[5] = SoftWare(mi, $"{aax} --Inform=General;%pub%", false); //Publisher
-            nfoPart[6] = SoftWare(mi, $"{aax} --Inform=General;%rldt%", false); //Release Date
-            nfoPart[7] = SoftWare(mi, $"{aax} --Inform=General;%Duration/String2%", false); //Duration (h, m)
-            nfoPart[8] = SoftWare(mi, $"{aax} --Inform=\"Menu;%FrameCount%\"", false); //Chapters
-            nfoPart[9] = SoftWare(mi, $"{aax} --Inform=General;%Format%", false); //general format
-            nfoPart[10] = SoftWare(mi, $"{m4b} --Inform=Audio;%Format%", false); //audio format
-            nfoPart[11] = SoftWare(mi, $"{m4b} --Inform=Audio;%BitRate%", false).Trim(); //source bitrate
             try
             {
-                nfoPart[11] = (Int32.Parse(nfoPart[11]) / 1000).ToString();
+                string mi = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe";
+                nfoPart[0] = SoftWare(mi, $"{aax} --Inform=General;%Album%", false); //Title
+                nfoPart[1] = SoftWare(mi, $"{aax} --Inform=General;%Performer%", false); //Author
+                nfoPart[2] = SoftWare(mi, $"{aax} --Inform=General;%nrt%", false); //Narrator
+                nfoPart[3] = SoftWare(mi, $"{aax} --Inform=General;%Copyright%", false); //Copyright
+                nfoPart[4] = SoftWare(mi, $"{aax} --Inform=General;%Genre%", false); //Genre
+                nfoPart[5] = SoftWare(mi, $"{aax} --Inform=General;%pub%", false); //Publisher
+                nfoPart[6] = SoftWare(mi, $"{aax} --Inform=General;%rldt%", false); //Release Date
+                nfoPart[7] = SoftWare(mi, $"{aax} --Inform=General;%Duration/String2%", false); //Duration (h, m)
+                nfoPart[8] = SoftWare(mi, $"{aax} --Inform=\"Menu;%FrameCount%\"", false); //Chapters
+                nfoPart[9] = SoftWare(mi, $"{aax} --Inform=General;%Format%", false); //general format
+                nfoPart[10] = SoftWare(mi, $"{m4b} --Inform=Audio;%Format%", false); //audio format
+                nfoPart[11] = SoftWare(mi, $"{m4b} --Inform=Audio;%BitRate%", false).Trim(); //source bitrate
+                try
+                {
+                    nfoPart[11] = (Int32.Parse(nfoPart[11]) / 1000).ToString();
+                }
+                catch
+                {
+                    nfoPart[11] = "125";
+                    AlertError("nfo Source Bitrate ERROR");
+                }
+                nfoPart[12] = SoftWare(mi, $"{m4b} --Inform=General;%CodecID%", false); //encoded codecID
+                nfoPart[13] = SoftWare(mi, $"{m4b} --Inform=Audio;%BitRate%", false); //encoded bitrate
+                try
+                {
+                    nfoPart[13] = (Int32.Parse(nfoPart[13]) / 1000).ToString();
+                }
+                catch
+                {
+                    nfoPart[11] = "125";
+                    AlertError("nfo Output Bitrate ERROR");
+                }
+                nfoPart[14] = SoftWare(mi, $"{aax} --Inform=General;%Track_More%", false); //comment
             }
-            catch
+            catch (Exception ex)
             {
-                nfoPart[11] = "125";
-                AlertError("nfo Source Bitrate ERROR");
+                AlertError("nfo ERROR");
+                AlertError(ex.Message);
             }
-            nfoPart[12] = SoftWare(mi, $"{m4b} --Inform=General;%CodecID%", false); //encoded codecID
-            nfoPart[13] = SoftWare(mi, $"{m4b} --Inform=Audio;%BitRate%", false); //encoded bitrate
-            try
-            {
-                nfoPart[13] = (Int32.Parse(nfoPart[13]) / 1000).ToString();
-            }
-            catch
-            {
-                nfoPart[11] = "125";
-                AlertError("nfo Output Bitrate ERROR");
-            }
-            nfoPart[14] = SoftWare(mi, $"{aax} --Inform=General;%Track_More%", false); //comment
 
             string nfo = @$"General Information
 ===================
@@ -93,13 +101,12 @@ Publisher's Summary
         {
             string mi = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe";
             string[] info = new string[5];
-            
+
             info[0] = SoftWare(mi, $"{aax} --Inform=General;%Album%", false);
             info[1] = info[0].Split(",").Last().Trim();
             info[2] = info[0].Replace(info[1], null).Replace(",", null).Split(":").Last().Trim();
             info[3] = info[0].Replace(info[1], null).Replace(info[2], null).Replace(":", null).Replace(",", null).Trim();
             info[4] = SoftWare(mi, $"{aax} --Inform=General;%Track_More%", false);
-
             info[1] = info[1].Replace(" ", null).Replace("Book", null);
 
             if (info[1].Contains("Volume"))
@@ -115,11 +122,9 @@ Publisher's Summary
             {
                 info[1] = String.Concat("0", info[1]);
             }
-
             return info;
         }
-
-
+        
         public static void Alert(string alert)
         {
             Console.Write("  [");
@@ -159,19 +164,6 @@ Publisher's Summary
             }
         }
 
-        /*public static void cue(string m4b, string file) //UNTHREADED METHOD
-        {
-            Alert("Generating cue");
-            SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $" -i \"{m4b}.m4b\" -c copy {AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv -y", true);
-            SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mkvextract.exe", $" {AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv chapters -s {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt", true);
-            File.Delete($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv");
-            string cuegen = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\cuegen.vbs {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt";
-            Process.Start(@"cmd", @"/c " + cuegen);
-            string[] cue = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.cue");
-            cue[0] = $"FILE \"{Path.GetFileName($"{file}.m4b")}\" MP4";
-            File.WriteAllLines($"{file}.cue", cue);
-        }*/
-
         public static void cueGenTHR(string aax, string file) //THREADED METHOD
         {
             Alert("Generating cue");
@@ -181,7 +173,7 @@ Publisher's Summary
             File.Delete($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\temp.mkv");
             string cuegen = $"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\cuegen.vbs {AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.txt";
             Process.Start(@"cmd", @"/c " + cuegen);
-            Thread.Sleep(360); // Give time for the conversion, thread suns over it otherwise. 360ms just to be sure, 1000 is advised
+            Thread.Sleep(700); //1000 is advised
             string[] cue = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\chapters.cue");
             cue[0] = $"FILE \"{Path.GetFileName($"{file}.m4b")}\" MP4";
             File.WriteAllLines($"{file.Replace("\"", "")}.cue", cue);
@@ -195,7 +187,6 @@ Publisher's Summary
             }
         }
 
-        //THREADED METHOD (NEW)
         public static void m4bMuxer(string bytes, string aax, string title, string comment, string file)
         {
             Alert("Muxing m4b");
@@ -215,8 +206,8 @@ Publisher's Summary
         public static string getBytes(string aax)
         {
             string checksum = SoftWare($@"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffprobe.exe", $"{aax}", true);
-            File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\_temp.txt", checksum.Replace(" ", ""));
-            string[] line = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\_temp.txt");
+            File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\checksum.txt", checksum.Replace(" ", ""));
+            string[] line = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}src\\data\\checksum.txt");
             checksum = (line[11].Split("==").Last());
 
             Alert($"Checksum: {checksum}");
@@ -303,23 +294,43 @@ namespace Main
                 return 0;
             }
 
+            string[] filename;
+            string comment;
+            string title;
+            string file;
+            string hostDir = Path.GetDirectoryName(aax).Replace("\"", "");
 
             Workings.Methods.Alert("Parsing File");
-            string[] filename = Workings.Methods.MediaInfo(aax);
-            string title = filename[0];
-            filename[0] = filename[0].Trim().Replace(":", " -");
-            string file = ($@"{filename[2]} [{filename[1]}] {filename[3]}");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"  {file}");
-            Console.ResetColor();
-            string comment = filename[4].Trim();
+            try
+            {
+                filename = Workings.Methods.MediaInfo(aax);
+                title = filename[0];
+                filename[0] = filename[0].Trim().Replace(":", " -");
+                file = ($@"{filename[2]} [{filename[1]}] {filename[3]}");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"  {file}");
+                Console.ResetColor();
+                comment = filename[4].Trim();
+                file = $"\"{hostDir}\\{filename[0]}\\{file.Trim()}";
+                System.IO.Directory.CreateDirectory($"{hostDir}\\{filename[0]}");
+            }
+            catch (Exception ex)
+            {
+                string info = Workings.Methods.SoftWare($"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe", $"{aax} --Inform=General;%Album%", false);
+                title = info;
+                info =info.Trim().Replace(":", " -");
+                file = info;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"  {file}");
+                Console.ResetColor();
+                comment = Workings.Methods.SoftWare($"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\mediainfo.exe", $"{aax} --Inform=General;%Track_More%", false);
+                file = $"\"{hostDir}\\{info}\\{file.Trim()}";
+                System.IO.Directory.CreateDirectory($"{hostDir}\\{info}");
+
+            }
+            
             string bytes = Workings.Methods.getBytes(aax);
 
-            //*** create AAX Host dir HERE before ffmpeg merge
-
-            string hostDir = Path.GetDirectoryName(aax).Replace("\"", "");
-            file = $"\"{hostDir}\\{filename[0]}\\{file.Trim()}";
-            System.IO.Directory.CreateDirectory($"{hostDir}\\{filename[0]}");
 
             Thread THR = new Thread(() => Workings.Methods.cueGenTHR($"{aax.Replace("\"", "")}", $"{file.Replace("\"", "")}"));
             Thread THR1 = new Thread(() => Workings.Methods.m4bMuxer(bytes, aax, title, comment, file.Replace("\"", "")));
@@ -327,18 +338,12 @@ namespace Main
             THR1.Start();
 
             THR1.Join();
-            //old unthreaded THR1
-            /*Workings.Methods.Alert("Muxing m4b");
-            Workings.Methods.SoftWare($"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $"-activation_bytes {bytes} -i {aax} -metadata:g encoding_tool=\"{Workings.iAmDeaf.mark} {Workings.iAmDeaf.version}\" -metadata title=\"{title}\" -metadata comment=\"{comment}\" -c copy {file}.m4b\" -y", true);
-            */
-
 
             Workings.Methods.Alert("Extracting jpg");
             Workings.Methods.SoftWare($"{AppDomain.CurrentDomain.BaseDirectory}src\\tools\\ffmpeg.exe", $"-i {aax} -map 0:v -map -0:V -c copy {file}.jpg\" -y", true);
             string nfo = Workings.Methods.nfo(aax, $"{file}.m4b\"");
 
-            //Workings.Methods.cue($"{file.Replace("\"", "")}", $"{file.Replace("\"", "")}");
-
+            
             THR.Join();
 
             Workings.Methods.Alert("Generating nfo");
